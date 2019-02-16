841352c6b9e32fb8b084e80d3089e455637be745
kevinjzheng <kjz233@nyu.edu>
2019-02-15 20:31:15 -0500

    app and requirements

---
+++ app.py
@@ -0,0 +1,16 @@
+import random
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask_pymongo import PyMongo
+import bcrypt
+import json
+from bson.objectid import ObjectId
+from datetime import datetime
+from bson.json_util import dumps, loads
+
+app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
+app.config["MONGO_URI"] = "mongodb://admin:password123@ds147942.mlab.com:47942/ks-hack"
+mongo = PyMongo(app)
+
+
+if __name__ == '__main__':
+    app.run()
